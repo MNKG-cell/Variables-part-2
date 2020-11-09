5f6e9e7f792f5bb4ccca721db404cad7f6ebe671
MAUREEN <maureennayebare2@gmail.com>
2020-11-09 09:40:58 +0300

    Variables 2

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('Hello World!');
+/*so here, the person is an object. Then the rest of the items are descriptions. For the last part, getElementById helps us to run the work*/
+var person={
+    name:'Nayebare Maureen', 
+    age:85,
+    hobbies:['reading Bible', 'Singing', 'Praying ', ]
+};
+document. getElementById('alt'). innerHTML=person. name+'<br>'+person.age+'<br>'+person.hobbies[1];
